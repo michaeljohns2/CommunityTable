fe6f9a253f9d1c981fd0d9df9f2bfe2b4d3ea4e5
root <root@ip-10-79-154-57.ec2.internal>
2016-11-01 00:42:08 +0000

    changed tomcat/conf,webapp,lib to source from host to docker container ..

--- platform/mongo_db.bash
+++ platform/mongo_db.bash
@@ -1,2 +1,2 @@
 [[ ! -a /usr/local/mongodb/data ]] && mkdir -p /usr/local/mongodb/data
-docker run --name ct-mongo --network=ct-network -h mongodb -p 27017:27017 -d -v /usr/local/mongodb/data:/data/db -d mongo:latest
+docker run --name ct-mongodb --network=ct-network -h mongodb -p 27017:27017 -d -v /usr/local/mongodb/data:/data/db -d mongo:latest
