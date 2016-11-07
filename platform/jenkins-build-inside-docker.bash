be24337e0bf60c8b1e8e5468d2f4a0e776f84b58
root <root@ip-10-79-154-57.ec2.internal>
2016-11-07 21:33:48 +0000

    changes ..

--- platform/jenkins-build-inside-docker.bash
+++ platform/jenkins-build-inside-docker.bash
@@ -2,5 +2,4 @@ cd /tmp/CommunityTable/ && \
 git pull &&
 cd /tmp/CommunityTable/WebApp/ && \
 mvn clean install && \
-cp /tmp/CommunityTable/WebApp/target/CommunityTables.war /usr/local/tomcat/webapps/  &&
-cd /tmp/CommunityTable/platform/ && make restart-tomcat
+cp /tmp/CommunityTable/WebApp/target/CommunityTables.war /usr/local/tomcat/webapps/
