6de3f722c818edde420f64648e0279f168f27e51
Dairai <dnyabando@g.harvard.edu>
2016-11-08 01:13:07 -0500

    Email subscription option
    added functionality to handle submission of an email to the email
    subscription service

--- WebApp/src/main/webapp/resources/js/community.js
+++ WebApp/src/main/webapp/resources/js/community.js
@@ -1,3 +1,35 @@
 /**
  * Created by SRoeAir on 11/6/16.
  */
+
+function enterEmail() {
+    document.getElementById('emailSubmit').style.visibility = "visible";
+    document.getElementById('subscribe').style.visibility = "collapse";
+}
+
+$("#subscribeToNewsletter").submit(function(e) {
+
+    var emailAddr = document.getElementById('emailAddress').value;
+
+    $.ajax({
+        method:'POST',
+        cache:false,
+        url: '/CommunityTables/rest/emails.json',//$(this).attr('action'),
+        data: emailAddr,//{"emailAddress":"test.nyabando@gmail.com"},
+        contentType: 'multipart/form-data',
+        success:function(result)//we got the response
+        {
+            document.getElementById('emailSubmit').style.visibility = "collapse";
+            document.getElementById('subscribe').style.visibility = "visible";
+            document.getElementById('successfulSubscription').style.visibility = "visible";
+        },
+        error:function(exception){
+            document.getElementById('emailSubmit').style.visibility = "collapse";
+            document.getElementById('subscribe').style.visibility = "visible";
+            document.getElementById('failSubscription').style.visibility = "visible";
+        }
+
+    });
+    return false;
+});
+
