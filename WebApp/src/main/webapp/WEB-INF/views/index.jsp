30f01846ad82a0398d34ff588d16f411ad8f772c
Michael Johns <michael.johns2@gmail.com>
2016-11-12 12:04:57 -0500

    added a ping.js for use in community.js to raise  500 error to UI layer

--- WebApp/src/main/webapp/WEB-INF/views/index.jsp
+++ WebApp/src/main/webapp/WEB-INF/views/index.jsp
@@ -81,6 +81,7 @@
     <script type="text/javascript" src="webjars/jquery/3.1.1/dist/jquery.min.js"></script>
     <script type="text/javascript" src="webjars/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<%--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>--%>
+	<script type="text/javascript" src="resources/js/ping.js"></script>
 	<script type="text/javascript" src="resources/js/community.js"></script>
 </body>
  
--- WebApp/src/main/webapp/resources/js/community.js
+++ WebApp/src/main/webapp/resources/js/community.js
@@ -4,29 +4,43 @@
 
 $("#subscribeToNewsletter").submit(function(e) {
 
-    var emailAddr = document.getElementById('emailAddress').value;
+    /* Use a ping jquery addon ( from https://gist.github.com/jerone/3487795 ) to verify server is available.
+       Without this test, the ajax call fails in a way that is not detected. There may be a better way but this
+       works for now.
+    */
+    $.Ping("/CommunityTables/rest/emails.json" /*, optional timeout */).done(function (success, url, time, on) {
+        // console.log("ping done", arguments);
+        var emailAddr = document.getElementById('emailAddress').value;
 
-    $.ajax({
-        method:'POST',
-        cache:false,
-        url: '/CommunityTables/rest/emails.json',
-        data: emailAddr,
-        contentType: 'multipart/form-data',
-        success:function(result)//we got the response
-        {
-            $('#successfulSubscription').fadeIn(300).delay(10000).fadeOut(300);
-            document.getElementById('emailSubmit').style.visibility = "collapse";
-            document.getElementById('subscribe').style.visibility = "visible";
-        },
-        error:function(exception){
-            //push the exception response text into the fail block on index
-            jsonValue = jQuery.parseJSON( exception.responseText );
-            document.getElementById('failSubscription').getElementsByTagName("P").item(0).innerHTML = jsonValue.message;
+        $.ajax({
+            method:'POST',
+            cache:false,
+            url: '/CommunityTables/rest/emails.json',
+            data: emailAddr,
+            contentType: 'multipart/form-data',
+            success:function(result)//we got the response
+            {
+                document.getElementById('emailSubmit').style.visibility = "collapse";
+                $('#successfulSubscription').fadeIn(300).delay(10000).fadeOut(300);
+                // document.getElementById('subscribe').style.visibility = "visible";//what is this?
+            },
+            error:function(exception){
+                //push the exception response text into the fail block on index
+                jsonValue = jQuery.parseJSON( exception.responseText );
+                document.getElementById('failSubscription').getElementsByTagName("P").item(0).innerHTML = jsonValue.message;
 
-            $('#failSubscription').fadeIn(300).delay(10000).fadeOut(300);
-        }
+                $('#failSubscription').fadeIn(300).delay(10000).fadeOut(300);
+            }
+        });
 
+    }).fail(function (failure, url, time, on) {
+        console.log("ping fail", arguments);
+        //Had to hard-code this exception
+        document.getElementById('failSubscription').getElementsByTagName("P").item(0).innerHTML = "So sorry, we are experiencing issues connecting to data store to save your email. Please try again later!";
+
+        $('#failSubscription').fadeIn(300).delay(10000).fadeOut(300);
     });
+
     return false;
 });
 
