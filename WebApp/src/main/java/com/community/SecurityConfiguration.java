8a11b20c80692103265c6197cf199094ea4911aa
Kelly Doyle <Kelly Doyle>
2016-11-19 10:07:42 -0500

    Removed a comment

--- WebApp/src/main/java/com/community/SecurityConfiguration.java
+++ WebApp/src/main/java/com/community/SecurityConfiguration.java
@@ -25,7 +25,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // TODO: create an access denied page
         http.authorizeRequests()
                 .antMatchers("/admin/**").access("hasRole('ADMIN')")
                 .and().formLogin();
