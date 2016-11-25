cec0b73bad2d370073ad5ecd0f8f02477eeed1e6
Michael Johns <michael.johns2@gmail.com>
2016-11-25 17:25:31 -0500

    tail tomcat tweak and verified jquery.js must be in _header.jsp

--- platform/dev/tail-tomcat.sh
+++ platform/dev/tail-tomcat.sh
@@ -2,8 +2,8 @@
 
 # e.g. catalina.yyyy-MM-dd.log
 
-FILE="catalina.$1.log"
+DATE=`date +%Y-%m-%d`
+FILE="catalina.$DATE.log"
 
-echo "expects arg1 with yyyy-MM-dd"
 echo "... tailing $FILE "
 docker exec -it ct-tomcat bash -c "tail -f -n 10 /usr/local/tomcat/logs/$FILE"
